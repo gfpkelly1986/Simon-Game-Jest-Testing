a530280176f38c42cc9d30908e4525812769f94e
gfpkelly1986 <gfpkelly@gmail.com>
2022-08-28 12:27:16 +0000

    Completed Simon Game with TDD approach.

--- assets/scripts/game.js
+++ assets/scripts/game.js
@@ -56,8 +56,22 @@ function showTurns() {
 }
 
 
+function playerTurn() {
+    let i = game.playerMoves.length - 1;
+    if (game.currentGame[i] === game.playerMoves[i]){
+        if (game.currentGame.length == game.playerMoves.length){
+            game.score++;
+            showScore();
+            addTurn();
+        } 
+    } else {
+        alert('Wrong move!');
+        newGame();
+    }
+}
+
 
 
 // Exports need to be in curly braces when multiple items are being exported
-module.exports = {game, newGame, showScore, addTurn, lightsOn, showTurns};
+module.exports = {game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn};
 
--- assets/scripts/tests/game.test.js
+++ assets/scripts/tests/game.test.js
@@ -1,5 +1,7 @@
 // Notice the curly braces again!
-const {game, newGame, showScore, addTurn, lightsOn, showTurns} = require('../game');
+const {game, newGame, showScore, addTurn, lightsOn, showTurns, playerTurn} = require('../game');
+
+jest.spyOn(window, 'alert').mockImplementation(() => { });
 
 //This is the same code for all file imports needed for testing in a mock DOM.
 beforeAll(() => {
@@ -88,5 +90,15 @@ describe('gameplay works correctly', () => {
         game.turnNumber = 42;
         showTurns();
         expect(game.turnNumber).toBe(0);
+    });
+    test('should increment the score if the turn is correct', () => {
+        game.playerMoves.push(game.currentGame[0]);
+        playerTurn();
+        expect(game.score).toBe(1);
+    });
+    test('should call an alert if the move is wrong', () => {
+        game.playerMoves.push('wrong');
+        playerTurn();
+        expect(window.alert).toBeCalledWith('Wrong move!');
     });
 });
